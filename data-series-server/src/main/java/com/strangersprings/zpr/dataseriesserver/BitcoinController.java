8ee97c0ad4dc9a82ee4ee19eb8f49ca30c58613c
olabudzyn <ola.budz96@gmail.com>
2019-11-23 14:10:18 +0100

    add CrossOrigin

--- data-series-server/src/main/java/com/strangersprings/zpr/dataseriesserver/BitcoinController.java
+++ data-series-server/src/main/java/com/strangersprings/zpr/dataseriesserver/BitcoinController.java
@@ -3,6 +3,7 @@ package com.strangersprings.zpr.dataseriesserver;
 import com.strangersprings.zpr.dataseriesserver.bitcoin.BitcoinDTO;
 import com.strangersprings.zpr.dataseriesserver.bitcoin.BitcoinService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/bitcoin")
 public class BitcoinController {
 

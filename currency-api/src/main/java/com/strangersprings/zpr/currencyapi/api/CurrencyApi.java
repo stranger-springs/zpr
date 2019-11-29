5a05ccf0f866dadc8a5ccf737d56fe292b4a50ea
Mateusz <mateuszkp96@gmail.com>
2019-11-29 20:07:58 +0100

    Currency api moved to separate module

--- currency-api/src/main/java/com/strangersprings/zpr/currencyapi/api/CurrencyApi.java
+++ currency-api/src/main/java/com/strangersprings/zpr/currencyapi/api/CurrencyApi.java
@@ -1,6 +1,5 @@
-package com.strangersprings.zpr.client.api;
+package com.strangersprings.zpr.currencyapi.api;
 
-import com.strangersprings.zpr.client.model.CurrencyData;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
